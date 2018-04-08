d08834ccfc77aab09be4409188778e9ef525008e
Marco Morcote-Marger <marmorco@umich.edu>
2018-04-08 19:07:26 -0400

    implemented some new functionality

--- sql/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(user_id, refresh_token)
-VALUES
-    ('dachshunds120', 'AQCN49jumep1xtx2ebRLSqI6pZJVfQ7B7-oZ5WSKopJi_xuW0ZZbSBt9CEqse-TzR2TYAvg-TZWeD3Cx97yD7wLtd8kAlNdTphfRAmuvIlJH2H3x9Tcsimzr3bPf_XuhgEY');
